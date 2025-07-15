134516299b3ff44c5ab668ad9083b8eae0c52cfb
Anton Tobolkin <tobantal@gmail.com>
2025-07-15 16:57:50 +0300

    правки GoldenRatioFibCounter для сборки в Linux

--- hw-03-algebraic-algorithms/src/GoldenRatioFibCounter.cpp
+++ hw-03-algebraic-algorithms/src/GoldenRatioFibCounter.cpp
@@ -2,15 +2,16 @@
 #include <cmath>
 #include <string>
 #include <cstdint>
-#include <cstdlib> // для std::stoll
+#include <cstdlib> // для std::stoll\
+#include <math.h>
 
 
 std::string GoldenRatioFibCounter::compute(const std::string& n_str) const {
 	long long n = std::stoll(n_str);
 	
-	long double sqrt5 = std::sqrtl(5.0L);
+	long double sqrt5 = sqrtl(5.0L);
 	long double phi = (1.0L + sqrt5) / 2.0L;
 
-	long double result = std::round(std::powl(phi, n) / sqrt5);
+	long double result = round(powl(phi, n) / sqrt5);
 	return std::to_string(static_cast<uint64_t>(result));
 }
