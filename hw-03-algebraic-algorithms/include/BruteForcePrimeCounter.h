d4a691b86e2abe6a1a47e5a0a1d92d304fbfd015
Anton Tobolkin <tobantal@gmail.com>
2025-07-16 17:17:15 +0300

    правка кода в реализации BruteForcePrimeCounter

--- hw-03-algebraic-algorithms/include/BruteForcePrimeCounter.h
+++ hw-03-algebraic-algorithms/include/BruteForcePrimeCounter.h
@@ -5,4 +5,6 @@
 class BruteForcePrimeCounter : public Counter {
 public:
 	std::string compute(const std::string& n_str) const override;
+private:
+	bool is_prime(uint64_t n) const;
 };
--- hw-03-algebraic-algorithms/src/BruteForcePrimeCounter.cpp
+++ hw-03-algebraic-algorithms/src/BruteForcePrimeCounter.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <cstdlib>
 
-bool is_prime(uint64_t n) {
+bool BruteForcePrimeCounter::is_prime(uint64_t n) const {
 	if (n <= 1) return false;
 	for (uint64_t d = 2; d < n; ++d)
 		if (n % d == 0) return false;
@@ -13,6 +13,8 @@ std::string BruteForcePrimeCounter::compute(const std::string& n_str) const {
 	uint64_t n = std::stoll(n_str);
 	uint64_t count = 0;
 	for (uint64_t i = 2; i <= n; ++i)
-		if (is_prime(i)) ++count;
+		if (is_prime(i)) {
+			++count;
+		}
 	return std::to_string(count);
 }
